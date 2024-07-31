b4a430e6a0b33f013a549ec2fb271b5514508ea2
Francisco Gomes <arretadolabs@gmail.com>
2024-07-31 15:45:49 -0300

    Printing numbers odd in interval

---
+++ src/structure_repetition/NumbersOddRange.java
@@ -0,0 +1,11 @@
+package structure_repetition;
+
+public class NumbersOddRange {
+    public static void main(String[] args) {
+        for (int i = 0; i < 50; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
