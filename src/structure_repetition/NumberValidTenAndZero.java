494ab80d6c9bfa82aa7bb078d00621b1a99f868c
Francisco Gomes <arretadolabs@gmail.com>
2024-07-31 10:34:01 -0300

    Verify number is valid in between zero and ten

---
+++ src/structure_repetition/NumberValidTenAndZero.java
@@ -0,0 +1,27 @@
+package structure_repetition;
+
+import java.util.Scanner;
+
+/**
+ * Importing class Scanner for read data user input
+ */
+
+public class NumberValidTenAndZero {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int number;
+
+        while (true) {
+            System.out.println("Enter with number: ");
+            number = sc.nextInt();
+
+            if (number >= 0 && number <= 10) {
+                System.out.println("number is valid, congrats! " + number);
+                break;
+            } else {
+                System.out.println("number is invalid, try again!!");
+            }
+        }
+        sc.close();
+    }
+}
